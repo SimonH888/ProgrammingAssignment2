91abcb373ebb4fec62c00e160615469d56a5ff85
SimonH888 <simon@harch.name>
2019-11-14 20:32:31 +1000

    Updated makeCacheMatrix and cacheSolve functions to accept an invertible matrix, solve to get its inverse matrix and cache ther reulting matrix for later use.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The 'makeCacheMatrix' function creates a list that contains
+## a function to store and access a matrix and a companion object,
+## using the following mutator and accessor methods:
+## 1.  set the value of the source matrix
+## 2.  get the value of the source matrix
+## 3.  set the value of the companion object
+## 4.  get the value of the companion object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setcompanion <- function(companion) m <<- companion
+        getcompanion <- function() m
+        list(set = set, get = get,
+             setcompanion = setcompanion,
+             getcompanion = getcompanion)
 }
 
 
-## Write a short comment describing this function
+## The 'cacheSolve' function calculates the inverse of the
+## matrix created using the 'makeCaheMatrix' function above.
+## It assumes the matrix supplied is invertible.
+## 'cacheSolve' checks whether a companion (in this case the inverted matrix)
+## object has been cached and if so, returns the cached companion object.
+## If no cache is found, the inverse of the matrix is calculated
+## using solve() and cached for future use.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getcompanion()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setcompanion(m)
+        m
+        
 }
